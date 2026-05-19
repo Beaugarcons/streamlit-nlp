693a6f8430a321f34652c8bd9ffc3ecf90ad51a4
Beaugarcons <510582205@qq.com>
2026-05-19 23:59:43 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import streamlit as st
 
-from modules import A1, A2, A3, A4, A5, A6, A7, A8, A9
-
 st.set_page_config(
     page_title="NLP实验平台",
     layout="wide"
@@ -25,28 +23,37 @@ page = st.sidebar.selectbox(
 )
 
 if page == "词汇分析":
+    from modules import A1
     A1.run()
 
 elif page == "句法分析":
+    from modules import A2
     A2.run()
 
 elif page == "语义分析":
+    from modules import A3
     A3.run()
 
 elif page == "语义分析2":
+    from modules import A4
     A4.run()
 
 elif page == "篇章分析":
+    from modules import A5
     A5.run()
 
 elif page == "语言模型":
+    from modules import A6
     A6.run()
 
 elif page == "信息抽取":
+    from modules import A7
     A7.run()
 
 elif page == "机器翻译":
+    from modules import A8
     A8.run()
 
 elif page == "情感分析":
+    from modules import A9
     A9.run()
